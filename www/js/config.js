5738b7466a93df21c105298ad60d08ef315234b8
Evan <evanomondi7@gmail.com>
2019-12-09 14:50:35 +0300

    firs ever commit

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+/*Kartero App Configuration*/
+var krms_driver_config ={	
+	'ApiUrl':"http://urbanman.co.ke/driver/api",						
+	'DialogDefaultTitle':"Driver",    
+	'APIHasKey':"c2f71acf91386b74a9cb6a5c86183154",
+	'debug': false
+};
