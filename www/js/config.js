14dc68b10ca659a2f8863a6061c26ef4941c01b4
Evan <evanomondi7@gmail.com>
2019-12-12 16:48:31 +0300

    new

--- www/js/config.js
+++ www/js/config.js
@@ -1,6 +1,6 @@
 /*Kartero App Configuration*/
 var krms_driver_config ={	
-	'ApiUrl':"http://urbanman.co.ke/control/api",						
+	'ApiUrl':"https://errandsguy.com/control/api",						
 	'DialogDefaultTitle':"Your Errands App",    
 	'APIHasKey':"pk.eyJ1IjoiZXZhbm9tb25kaSIsImEiOiJjaXJ2MmV5NzcwMGZraGttMTBlaWR0djRhIn0.eoaOYRNwHu3m3KZY3rabfg",
 	'debug': false
