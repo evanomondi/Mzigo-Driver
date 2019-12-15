e53b1ea1bc56aec7e565c30c5fdd62b5c55c4199
Evan <evanomondi7@gmail.com>
2019-12-15 18:25:42 +0300

    new commit

--- www/js/config.js
+++ www/js/config.js
@@ -3,5 +3,5 @@ var krms_driver_config ={
 	'ApiUrl':"https://errandsguy.com/control/api",						
 	'DialogDefaultTitle':"Your Errands App",    
 	'APIHasKey':"fe7709c6106629a1fc4dd32745a0f592",
-	'debug': false
+	'debug': true
 };
